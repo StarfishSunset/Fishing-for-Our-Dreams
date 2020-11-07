26e6f8ee9c1798ae8657355277b3d0672e0a608c
StarfishSunset <72940354+StarfishSunset@users.noreply.github.com>
2020-11-07 11:57:46 +0800

    ttttext

--- sketch.js
+++ sketch.js
@@ -87,7 +87,7 @@ pop()
 fill(199,67,117)
 textSize(17)
 textFont('Papyrus')
-text('Move the boats left and right with the arrow keys', 30, 310);
+text('Move the boats left and right with the arrow keys', 30, 500);
 //END text
  
 fishing();
